e1afa4f200e00eb22455ed26e635ae8752ac7ead
SAURAV <SAURAV@DESKTOP-PRSE22V>
2023-02-20 11:40:46 +0530

    First Commit

---
+++ AssignmentTwo/src/org/tnsif/stringdemo.java
@@ -0,0 +1,13 @@
+package org.tnsif.stringdemo;
+class Sample{
+	
+}
+
+public class ObjectClassDemo {
+	public static void main(String[] args) {
+		Sample s=new Sample();
+		System.out.println(s.getClass());
+		System.out.println(s.hashCode());
+
+	}
+}
